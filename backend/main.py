a25d268c2db96b1e47ab0ce17bb724c99f9d896a
sdurna <serkandurna48@gmail.com>
2026-04-01 22:25:25 +0200

    fix cors for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://handwerker-tool-ter.vercel.app",
+        "https://handwerker-tool-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
